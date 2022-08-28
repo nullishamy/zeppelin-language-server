8ecdc6a67a55292b5f73a6fca23b8cb962c4e5eb
do not the cat <perhaps-you-know@what-is.ml>
2022-08-28 17:26:53 +0400

    feat: add `args` as a root value (#2)
    this adds `args` as a root value to support better completion

--- src/const/values.ts
+++ src/const/values.ts
@@ -26,6 +26,8 @@ export const VALUES: TagValue[] = [
 
 	{ key: 'member.joinedAt', type: 'number', kind: 'child' },
 
+	{ key: 'args', type: 'object', kind: 'root' },
+
 	// Args can have more values than this, though anything more is
 	// uncommon so it's not worth generating for
 	...Array.from({ length: 15 }).map((_, i) => ({
