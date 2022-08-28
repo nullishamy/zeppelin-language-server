cb6b5c1a981d383549030eac65a56e4b690bfa8f
do not the cat <perhaps-you-know@what-is.ml>
2022-08-28 09:01:49 +0400

    Update onDefinition.ts

--- src/event/onDefinition.ts
+++ src/event/onDefinition.ts
@@ -26,29 +26,39 @@ export function onDefinition(data: DefinitionParams): Definition | undefined {
 		// which we are filtering for
 		.filter((f) => f.namedChild(1)?.child(1)?.text === varname);
 
-	if (setNodes.length === 0) {
-		// TODO: diagnostic for `get` calls with no `set` call
+	const setrNodes = ast.rootNode
+		.descendantsOfType(['fn'])
+		// All `setr` calls
+		.filter((f) => f.namedChild(0)?.text === 'setr')
+		// Second child call to `1` to skip open_paren of the args list
+
+		// Setr calls come in the form `setr(varname, varvalue)` to the first arg is always the name
+		// which we are filtering for
+		.filter((f) => f.namedChild(1)?.child(1)?.text === varname);
+        const allNodes = (setNodes.length > 0 ? setNodes : setrNodes)
+
+        if(allNodes.length === 0) {
+		// TODO: diagnostic for `get` calls with no `set` or `setr` call
 		// TODO: improve this with per-document var maps?
 		// No definition found, return
 		return;
-	}
 
-	if (setNodes.length === 1) {
-		const node = setNodes[0];
+        if(allNodes.length === 1) {
+		const node = allNodes[0];
 		return {
 			uri,
 			range: Range.create(
-				// Wrap the whole set token
+				// Wrap the whole set or setr token
 				Position.create(node.startPosition.row, node.startPosition.column),
 				Position.create(node.startPosition.row, node.startPosition.column)
 			),
 		};
 	}
 
-	return setNodes.map((node) => ({
+	return allNodes.map((node) => ({
 		uri,
 		range: Range.create(
-			// Wrap the whole set token
+			// Wrap the whole set or setr token
 			Position.create(node.startPosition.row, node.startPosition.column),
 			Position.create(node.startPosition.row, node.startPosition.column)
 		),
